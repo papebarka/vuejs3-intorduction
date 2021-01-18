d0cbbd6cd4ab919c123912c94eb6ebd4daccadbd
Pape Barka Seydou Coulibaly <papebarka@gmail.com>
2021-01-18 17:14:49 +0000

    Ordering mistake

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return {
             product: 'shoes',
-            description: 'Nike Air Jordan 980s'
+            description: 'Nike Air Jordan 980s',
+            image: './assets/images/socks_green.jpg',
+            url: 'www.amazon.com'
         }
     }
 })
