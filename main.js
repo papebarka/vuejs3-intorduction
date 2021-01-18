cbf9cff5dca4f731214cc226c480ef1d40858d7b
Pape Barka Seydou Coulibaly <papebarka@gmail.com>
2021-01-18 18:05:56 +0000

    L5 done

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ const app = Vue.createApp({
             product: 'shoes',
             description: 'Nike Air Jordan 980s',
             image: './assets/images/socks_green.jpg',
-            inventory: 100,
-            onSale: false,
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% Polyester'],
+            variants: [
+                { id: 2234, color: 'Green' },
+                { id: 2235, color: 'Blue' }
+            ],
+            sizes: ['32', '34', '34', '40', '45'],
         }
     }
 })
